5a43198cb9dddab967c2d1e6379be7345a044801
tracyyh <huang.tr@northeastern.edu>
2023-09-15 10:07:28 -0400

    added in progress notice

--- src/app.js
+++ src/app.js
@@ -6,7 +6,6 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('hi carolyn');
 });
 
 server.listen(port, hostname, () => {
